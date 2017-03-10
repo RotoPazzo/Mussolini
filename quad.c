44d0a0a91553363ae43a32546e73ce71bdd3da44
fuomag9 <fuomag9@hotmail.it>
2017-03-10 22:30:10 +0100

    fixed code

--- quad.c
+++ quad.c
@@ -1,18 +1,17 @@
-#include<stdio.h> 
+#include<stdio.h>
 #include<stdlib.h>
 #include <math.h>
-void quadrato( int lato);{
-     float diagonale;
-     diagonale=lato*sqrt(2);
-     printf("la diagonale vale %.2f\n",diagonale);
-    printf("il perimetro vale %d",lato*4);
-    printf("l\'area vale %d",lato*lato);}
-main() {
-	int a;
-	do { 
-	printf("inserisci il lato a:\n");
-	scanf("%d",&a);
-} while(a<=0);
+void quadrato( float lato){
+float diagonale;
+diagonale=lato*sqrt(2);
+printf("la diagonale vale %f\n",diagonale);
+printf("il perimetro vale %f\n",lato*4);
+printf("l\'area vale %f\n",lato*lato);}
+int main() {
+float a;
+do {
+printf("inserisci il lato:\n");
+scanf("%f",&a);
+}while(a<=0);
 quadrato(a);
-system("PAUSE");
 }
